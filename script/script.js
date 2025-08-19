f48f27d8115f3055ea58b0c91f1703cb3107b49f
VLADIMIR <91013376+SeoVizor@users.noreply.github.com>
2025-08-19 19:55:14 +0300

    19.08

---
+++ script/script.js
@@ -0,0 +1,10 @@
+console.log("Внешний скрипт подключен")
+
+console.group("Группа логов");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+console.groupEnd();
