7165e1cd436eb50465d6439a7bc4137263febde1
VLADIMIR <91013376+SeoVizor@users.noreply.github.com>
2025-08-20 21:52:13 +0300

    игра

--- script/script.js
+++ script/script.js
@@ -1,10 +1,46 @@
-console.log("Внешний скрипт подключен")
 
-console.group("Группа логов");
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4");
-console.log("5");
+var x = prompt("Введите кординату", "");
 
-console.groupEnd();
+ var input = parseInt(x);
+ var hod = input;
+
+
+var [location1, location2, location3] = Array.from({length: 7}, (_, i) => i + 1)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 3)
+    .sort((a, b) => a - b);
+var isSunk = false;
+var guesses = 0;
+
+
+
+  if (location1 === input || location2 === input || location3 === input) 
+    {
+         
+       guesses++;
+       alert ('Попал')
+       var x = prompt("Введите следующую кординату", "");
+       var input = parseInt(x);
+       var hodtwo = input;
+         if(input != hod){
+           if (location1 === input || location2 === input || location3 === input) 
+            {
+               guesses++;
+               alert ('Попал')
+               var x = prompt("Введите следующую кординату", "");
+               var input = parseInt(x);
+               
+             if(input != hodtwo){
+               if (location1 === input || location2 === input || location3 === input) 
+                {
+                   guesses++;
+                   alert ('потоплен');
+                  
+                   // Тут сценарий закончен все три попадания
+                }
+            }
+            }
+            }
+    } else {alert ('не попал')}
+
+ 
